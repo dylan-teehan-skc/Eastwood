8007c03b560ae2fb52eda314902d4f40ba3f5232
sloggo <switchbyteyt@gmail.com>
2025-05-20 09:26:54 +0100

    add crypto++ and boilerplate code for hkdf

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
-#include <QApplication>
-#include <QPushButton>
+#include <iostream>
+#include <string>
+
+// Crypto++ includes
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hkdf.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
+
+using namespace std;
+using namespace CryptoPP;
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    CryptoPP::byte password[] = "password";
+    size_t plen = strlen((const char*)password);
+
+    CryptoPP::byte salt[] = "salt";
+    size_t slen = strlen((const char*)salt);
+
+    CryptoPP::byte info[] = "HKDF key deriv";
+    size_t ilen = strlen((const char*)info);
+
+    CryptoPP::byte derived[CryptoPP::SHA1::DIGESTSIZE];
+
+    HKDF<SHA1> hkdf;
+    hkdf.DeriveKey(derived, sizeof(derived), password, plen, salt, slen, info, ilen);
+
+    string result;
+    HexEncoder encoder(new StringSink(result));
+
+    encoder.Put(derived, sizeof(derived));
+    encoder.MessageEnd();
+    
+    cout << "HKDF Result: " << result << endl;
+
+    return 0;
 }
