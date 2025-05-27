16f7e3b620ddae162f732944f73b54feb706c0ba
Fred Sheppard <frederickjssheppard@gmail.com>
2025-05-27 13:47:17 +0100

    Create github action to run tests (#56)
    * Create test action
    
    * Include vector

--- src/utils/ConversionUtils.h
+++ src/utils/ConversionUtils.h
@@ -2,7 +2,7 @@
 #define CONVERSION_UTILS_H
 
 #include <string>
-#include <stdexcept>
+#include <vector>
 
 bool hex_to_bin(const std::string& hex, unsigned char* bin, size_t bin_size);
 std::string bin_to_hex(const unsigned char* bin, size_t bin_size);
