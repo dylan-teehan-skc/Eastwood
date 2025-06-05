815d392be4b5cb7b4171576417c5565028e06a13
Dylan Teehan <96421021+dylan-teehan-skc@users.noreply.github.com>
2025-06-05 22:17:28 +0100

    template class (#215)

---
+++ src/utils/Cache.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <optional>
+#include <chrono>
+
+namespace utils {
+
+template<typename T>
+class Cache {
+private:
+    struct CacheEntry {
+        T data;
+        std::chrono::system_clock::time_point expiry;
+    };
+
+    std::unordered_map<std::string, CacheEntry> cache;
+    std::chrono::seconds default_ttl;
+
+public:
+    explicit Cache(std::chrono::seconds ttl = std::chrono::seconds(300)) 
+        : default_ttl(ttl) {}
+
+    void set(const std::string& key, const T& value, 
+             std::chrono::seconds ttl = std::chrono::seconds(0)) {
+        auto expiry = std::chrono::system_clock::now() + 
+                     (ttl.count() > 0 ? ttl : default_ttl);
+        cache[key] = {value, expiry};
+    }
+
+    std::optional<T> get(const std::string& key) {
+        auto it = cache.find(key);
+        if (it == cache.end()) {
+            return std::nullopt;
+        }
+
+        if (std::chrono::system_clock::now() > it->second.expiry) {
+            cache.erase(it);
+            return std::nullopt;
+        }
+
+        return it->second.data;
+    }
+
+    void remove(const std::string& key) {
+        cache.erase(key);
+    }
+
+    void clear() {
+        cache.clear();
+    }
+
+    bool exists(const std::string& key) {
+        auto it = cache.find(key);
+        if (it == cache.end()) {
+            return false;
+        }
+
+        if (std::chrono::system_clock::now() > it->second.expiry) {
+            cache.erase(it);
+            return false;
+        }
+
+        return true;
+    }
+};
+
+} // namespace utils 
