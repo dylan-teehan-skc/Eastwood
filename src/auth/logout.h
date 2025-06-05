16ee6a6b8bfd1e0f287399bec59b5ec0747d3b11
Fred Sheppard <frederickjssheppard@gmail.com>
2025-06-05 22:17:39 +0100

    Logout user serverside (#216)

--- src/auth/logout.h
+++ src/auth/logout.h
@@ -4,8 +4,10 @@
 #include "src/database/database.h"
 #include "src/keys/kek_manager.h"
 #include "src/keys/session_token_manager.h"
+#include "src/client_api_interactions/MakeAuthReq.h"
 
 inline void logout() {
+    get("/logout");
     Database::get().closeDatabase();
     KekManager::instance().unload();
     SessionTokenManager::instance().clearToken();
